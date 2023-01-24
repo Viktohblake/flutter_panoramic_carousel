753bcb78a8c11d7f93b442171bdeb9b3beefef2b
viktohblake <damanygt@gmail.com>
2023-01-24 22:44:18 +0100

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -7,10 +7,13 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Text('Paranomic Carousel'),
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(child: Text('Paranomic Carousel')),
+      ),
     );
   }
 }
