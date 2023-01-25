68133413abc2614eb3d485cb43aea3d4474db0bb
viktohblake <damanygt@gmail.com>
2023-01-25 14:51:56 +0100

    change package name from 'paranomic_carousel' to 'panaromic_carousel'

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "paranomic_carousel");
+    gtk_header_bar_set_title(header_bar, "panaromic_carousel");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "paranomic_carousel");
+    gtk_window_set_title(window, "panaromic_carousel");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:paranomic_carousel/main.dart';
+import 'package:panaromic_carousel/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
