21e38eee4734350d1df9225b9d26bf5b9822ce4a
viktohblake <damanygt@gmail.com>
2023-01-25 11:47:53 +0100

    make carousel looks nice with viewport

--- lib/paranomic_carousel.dart
+++ lib/paranomic_carousel.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 
 class ParanomicCarousel extends StatefulWidget {
   const ParanomicCarousel({super.key});
@@ -9,8 +9,50 @@ class ParanomicCarousel extends StatefulWidget {
 }
 
 class _ParanomicCarouselState extends State<ParanomicCarousel> {
+  late final PageController pageController;
+
+  @override
+  void initState() {
+    pageController = PageController(initialPage: 0, viewportFraction: 0.85);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: Column(children: [
+        SizedBox(
+          height: 200,
+          child: ScrollConfiguration(
+            behavior: ScrollConfiguration.of(context).copyWith(dragDevices: {
+              PointerDeviceKind.touch,
+              PointerDeviceKind.mouse
+            }),
+            child: PageView.builder(
+              controller: pageController,
+              itemBuilder: (_, index) {
+                return AnimatedBuilder(
+                  animation: pageController,
+                  builder: (context, child) {
+                    return child!;
+                  },
+                  child: Container(
+                    margin: EdgeInsets.only(top: 20, left: 8, right: 8),
+                    color: Colors.amberAccent,
+                  ),
+                );
+              },
+              itemCount: 5,
+            ),
+          ),
+        )
+      ]),
+    );
   }
 }
