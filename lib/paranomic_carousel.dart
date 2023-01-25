3c32d62469b4b6197b21b9110b22a49da5119586
viktohblake <damanygt@gmail.com>
2023-01-25 10:53:09 +0100

    add paranmic carousel

---
+++ lib/paranomic_carousel.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ParanomicCarousel extends StatefulWidget {
+  const ParanomicCarousel({super.key});
+
+  @override
+  State<ParanomicCarousel> createState() => _ParanomicCarouselState();
+}
+
+class _ParanomicCarouselState extends State<ParanomicCarousel> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
