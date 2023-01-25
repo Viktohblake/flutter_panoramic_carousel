72d788035293ab3e4d52ba137c27e53c72a97541
viktohblake <damanygt@gmail.com>
2023-01-25 11:47:16 +0100

    add paranomic carousel slider indicator file

---
+++ lib/paranomic_carousel_indicator.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ParanomicCarouselIndicator extends StatefulWidget {
+  const ParanomicCarouselIndicator({super.key});
+
+  @override
+  State<ParanomicCarouselIndicator> createState() =>
+      _ParanomicCarouselIndicatorState();
+}
+
+class _ParanomicCarouselIndicatorState
+    extends State<ParanomicCarouselIndicator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
